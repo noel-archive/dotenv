687e148a6e5c26eba377c37f566906cf4f353dd1
Noel <augu@voided.pw>
2020-07-31 14:16:15 -0700

    im bad

--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,25 @@
-const { parse } = require('../lib');
+const { parse, TypeReader } = require('../lib');
  
+const MyTypeReader = (class MyTypeReader extends TypeReader {
+  constructor() {
+    super('uwu');
+  }
+
+  validate(arg) {
+    if (arg === 'uwu') return true;
+    else return false;
+  }
+
+  parse(arg) {
+    return arg;
+  }
+});
+
 const result = parse({
   populate: false,
+  readers: [MyTypeReader],
   schema: {
-    VALUE1: 'string',
+    VALUE1: 'uwu',
     VALUE2: 'boolean',
     VALUE3: 'int',
     VALUE4: 'float',
